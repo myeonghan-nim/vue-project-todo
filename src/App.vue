1164d914ecd181c92b3226702e1585e5f0868db1
Myeonghan Jeong <wjdaudgks94@naver.com>
2019-11-06 14:19:33 +0900

    Add: todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginForm/>
+    <TodoList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+import LoginForm from './components/LoginForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList,
+    LoginForm
   }
 }
 </script>
